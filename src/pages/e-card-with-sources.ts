8fd2bbe4f122569cfc2af3db8200bca80299dfe6
shonya3 <poeshonya3@gmail.com>
2023-12-21 18:28:03 +0300

    sort sources by level

--- src/pages/e-card-with-sources.ts
+++ src/pages/e-card-with-sources.ts
@@ -1,12 +1,13 @@
 import { styleMap } from 'lit/directives/style-map.js';
-import { LitElement, css, html } from 'lit';
-import { customElement, property } from 'lit/decorators.js';
+import { LitElement, PropertyValueMap, css, html } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
 import type { CardSize } from '../elements/divination-card/e-divination-card';
-import { poeData } from '../PoeData';
+import { PoeData, poeData } from '../PoeData';
 import { SourcefulDivcordTable } from '../divcord';
 import type { RenderMode } from '../elements/types';
 import '../elements/divination-card/e-divination-card';
 import '../elements/e-source';
+import { ISource } from '../ISource.interface';
 
 declare global {
 	interface HTMLElementTagNameMap {
@@ -22,6 +23,16 @@ export class CardWithSourcesElement extends LitElement {
 	@property({ type: Object }) divcordTable!: SourcefulDivcordTable;
 	@property() renderMode: RenderMode = 'compact';
 
+	@state() sources: ISource[] = [];
+
+	protected willUpdate(map: PropertyValueMap<this>): void {
+		if (map.has('name') || map.has('divcordTable')) {
+			const sources = this.divcordTable.sourcesByCard(this.name);
+			sortSourcesByLevel(sources, poeData);
+			this.sources = sources;
+		}
+	}
+
 	render() {
 		const wrapperStyles = styleMap({
 			'--card-width': `var(--card-width-${this.size})`,
@@ -35,7 +46,7 @@ export class CardWithSourcesElement extends LitElement {
 					minLevel=${this.minLevel ?? poeData.minLevel(this.name)}
 				></e-divination-card>
 				<ul class="sources">
-					${this.divcordTable.sourcesByCard(this.name).map(source => {
+					${this.sources.map(source => {
 						return html`<e-source
 							renderMode=${this.renderMode}
 							.source=${source}
@@ -65,4 +76,45 @@ export class CardWithSourcesElement extends LitElement {
 			margin-top: 0.25rem;
 		}
 	`;
+}
+
+function sourceLevel(source: ISource, poeData: Readonly<PoeData>): number | null {
+	switch (source.type) {
+		case 'Act': {
+			return poeData.level(source.id, source.type);
+		}
+		case 'Act Boss': {
+			const b = poeData.findActbossAndArea(source.id);
+			if (!b) return null;
+			return b.area.areaLevel;
+		}
+		case 'Map': {
+			return poeData.level(source.id, source.type);
+		}
+		case 'Map Boss': {
+			const b = poeData.mapboss(source.id);
+			if (!b) return null;
+			const mapLevels: number[] = [];
+			for (const map of b.maps) {
+				const level = poeData.level(map, 'Map');
+				if (level !== null) {
+					mapLevels.push(level);
+				}
+			}
+			const minLevel = Math.min(...mapLevels);
+			return minLevel === Infinity ? null : minLevel;
+		}
+		default: {
+			return null;
+		}
+	}
+}
+
+function sortSourcesByLevel(sources: ISource[], poeData: Readonly<PoeData>): void {
+	sources.sort((s1, s2) => {
+		// if source has no level, put it to the end
+		const level1 = sourceLevel(s1, poeData) ?? 200;
+		const level2 = sourceLevel(s2, poeData) ?? 200;
+		return level1 - level2;
+	});
 }
