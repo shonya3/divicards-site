900e5ba3504e3c4e0c9fc4e1f6245cc87e5915fc
shonya3 <poeshonya3@gmail.com>
2023-12-25 13:14:56 +0300

    drop vite-top-level-await and set build target es2022

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
-import topLevelAwait from 'vite-plugin-top-level-await';
 import { version } from './package.json';
 
 export default defineConfig({
-	plugins: [topLevelAwait()],
 	define: {
 		'import.meta.env.PACKAGE_VERSION': JSON.stringify(version),
+	},
+	build: {
+		target: 'ES2022',
 	},
 });
