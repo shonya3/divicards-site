31d2b75d3a36a453559b24720c117109ecdbed92
shonya3 <poeshonya3@gmail.com>
2024-02-05 18:43:06 +0300

    prepare need-to-verify for border transition

--- src/elements/e-need-to-verify.ts
+++ src/elements/e-need-to-verify.ts
@@ -1,52 +1,110 @@
 import { LitElement, css, html } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, query } from 'lit/decorators.js';
 
 declare global {
 	interface HTMLElementTagNameMap {
 		'e-need-to-verify': NeedToVerifyElement;
+		'e-need-to-verify-border': NeedToVerifyBorderElement;
 	}
 }
 
-@customElement('e-need-to-verify')
 /**
- * Wrap the drop source that needed to be verified.
- * ```html
- *  <e-need-to-verify>
- *      <e-source .source=${source}></e-source>
- *  </e-need-to-verify>
+ * @summary Notifiyng border for e-divination-card and e-source, when it needs to be verified.
+ * Example:
+ * ```js
+ * <e-need-to-verify>
+ *  <e-divination-card name="The Doctor"></e-divination-card>
+ * </e-need-to-verify>
  * ```
  */
+@customElement('e-need-to-verify')
 export class NeedToVerifyElement extends LitElement {
-	render() {
-		return html` <p class="label">need to verify</p>
-			<div class="border">
+	@query('.slot-parent') slotParent!: HTMLDivElement;
+
+	protected render() {
+		return html`
+			<div class="slot-parent">
 				<slot></slot>
-			</div>`;
+			</div>
+			<e-need-to-verify-border></e-need-to-verify-border>
+		`;
 	}
 
 	static styles = css`
-		:host {
-            --color: var(--sl-color-teal-600, teal);
-		}
-
 		* {
 			padding: 0;
 			margin: 0;
+			box-sizing: border-box;
 		}
 
-		.label {
-			text-align: end;
-			color: var(--color);
-            font-size: 0.75rem;
-		}
-
-		.border {
+		:host {
 			display: block;
-			padding-inline: 1rem;
-            padding-block: 0.4rem;
-			border-radius: 0.5rem;
 			position: relative;
+			width: fit-content;
+			height: fit-content;
+
+			display: flex;
+			justify-content: center;
+			align-items: center;
+
+			border-top: 16px solid transparent;
+		}
+
+		.slot-parent {
+			padding-inline: var(--padding-inline, 0rem);
+			padding-block: var(--padding-block, 0rem);
+		}
+
+		e-border {
+			box-sizing: content-box;
+		}
+
+		/** hint which tags are expected */
+		::slotted(:is(e-divination-card, e-source, *)) {
+			--padding-inline: 1rem;
+			--padding-block: 0.4rem;
+		}
+	`;
+}
+
+@customElement('e-need-to-verify-border')
+export class NeedToVerifyBorderElement extends LitElement {
+	protected render() {
+		return html`<p>Need to verify</p>`;
+	}
+
+	static styles = css`
+		* {
+			padding: 0;
+			margin: 0;
+			box-sizing: border-box;
+		}
+
+		:host {
+			--color: var(--sl-color-teal-600, teal);
+
+			width: calc(100%);
+			height: calc(100%);
+
+			position: absolute;
+			top: 50%;
+			left: 50%;
+			translate: -50% -50%;
+
+			border-radius: 0.5rem;
 			box-shadow: 4px 4px var(--color);
-			box-shadow: 1px 1px 5px  var(--color)
+			box-shadow: 1px 1px 5px var(--color);
+		}
+
+		p {
+			position: absolute;
+			top: 0;
+			right: 0;
+			font-size: 0.75rem;
+			translate: 0 -100%;
+
+			white-space: nowrap;
+			color: var(--color);
+		}
 	`;
 }
