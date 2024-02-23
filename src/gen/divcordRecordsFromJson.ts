b8c8deb375bc96d56eceb52ab21be1fe580119ab
shonya3 <poeshonya3@gmail.com>
2024-02-23 08:22:35 +0300

    change DivcordRecord to type

--- src/gen/divcordRecordsFromJson.ts
+++ src/gen/divcordRecordsFromJson.ts
@@ -2,6 +2,21 @@ import { ISource } from './ISource.interface';
 import json from './records.json';
 export const divcordRecordsFromJson = json as DivcordRecord[];
 
+/** Represents one row from divcord spreadsheet https://docs.google.com/spreadsheets/d/1Pf2KNuGguZLyf6eu_R0E503U0QNyfMZqaRETsN5g6kU/edit?pli=1#gid=0  */
+export type DivcordRecord = {
+	id: number;
+	card: string;
+	greynote: IGreynote;
+	tagHypothesis?: string;
+	confidence: IConfidence;
+	remainingWork: IRemainingWork;
+	wikiDisagreements?: string;
+	sourcesWithTagButNotOnWiki?: string;
+	verifySources: ISource[];
+	notes?: string;
+	sources?: ISource[];
+};
+
 export type IGreynote = (typeof GREYNOTE_VARIANTS)[number];
 export type IConfidence = (typeof CONFIDENCE_VARIANTS)[number];
 export type IRemainingWork = (typeof REMAINING_WORK_VARIANTS)[number];
@@ -28,18 +43,3 @@ export const REMAINING_WORK_VARIANTS = [
 	'open ended',
 ] as const;
 export const CONFIDENCE_VARIANTS = ['none', 'low', 'ok', 'done'] as const;
-
-/** Represents one row from divcord spreadsheet https://docs.google.com/spreadsheets/d/1Pf2KNuGguZLyf6eu_R0E503U0QNyfMZqaRETsN5g6kU/edit?pli=1#gid=0  */
-export interface DivcordRecord {
-	id: number;
-	card: string;
-	greynote: IGreynote;
-	tagHypothesis?: string;
-	confidence: IConfidence;
-	remainingWork: IRemainingWork;
-	wikiDisagreements?: string;
-	sourcesWithTagButNotOnWiki?: string;
-	verifySources: ISource[];
-	notes?: string;
-	sources?: ISource[];
-}
