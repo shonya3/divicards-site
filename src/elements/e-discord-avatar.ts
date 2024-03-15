e107036d0e42d9ae5592e9d559bd02a99fe08ea4
shonya3 <poeshonya3@gmail.com>
2024-03-15 04:53:55 +0300

    add gen avatars

---
+++ src/elements/e-discord-avatar.ts
@@ -0,0 +1,57 @@
+import { LitElement, html, css, PropertyValueMap } from 'lit';
+import { customElement, property, state } from 'lit/decorators.js';
+import { DISCORD_AVATARS } from '../gen/avatars';
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'e-discord-avatar': DiscordAvatarElement;
+	}
+}
+
+@customElement('e-discord-avatar')
+export class DiscordAvatarElement extends LitElement {
+	@property({ reflect: true }) username: keyof typeof DISCORD_AVATARS = 'nerdyjoe';
+	@property({ type: Number, reflect: true }) size = 40;
+	@state() src: string =
+		'https://cdn.discordapp.com/avatars/212041922150137857/ed0f38962063b40da72b39db7662c3bf.webp';
+	@state() color: string = '#fff';
+
+	protected willUpdate(map: PropertyValueMap<this>): void {
+		if (map.has('username')) {
+			const obj = DISCORD_AVATARS[this.username as keyof typeof DISCORD_AVATARS];
+			if (obj) {
+				this.src = `${obj.url}?size=${this.size}`;
+				this.color = obj.color;
+			}
+		}
+
+		if (map.has('color')) {
+			this.style.setProperty('--color', this.color);
+		}
+	}
+
+	protected render() {
+		return html`<img width=${this.size} height=${this.size} src=${this.src} alt="Discord Avatar" /> ${this
+				.username}`;
+	}
+
+	static styles = css`
+		* {
+			padding: 0;
+			margin: 0;
+			box-sizing: border-box;
+		}
+
+		:host {
+			color: var(--color);
+			display: flex;
+			gap: 0.5rem;
+			align-items: center;
+			width: fit-content;
+		}
+
+		img {
+			border-radius: 50%;
+		}
+	`;
+}
