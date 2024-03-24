9446965ab4d531013aeef73208686a7d6f869bea
shonya3 <poeshonya3@gmail.com>
2024-03-24 19:07:00 +0300

    add sort utils

---
+++ src/elements/divcord-spreadsheet/Sort.ts
@@ -0,0 +1,15 @@
+import { DivcordRecordAndWeight } from './e-divcord-spreadsheet';
+
+export type Order = 'asc' | 'desc';
+
+export class Sort {
+	static byName(cards: DivcordRecordAndWeight[], order: Order): void {
+		cards.sort((a, b) => (order === 'asc' ? a.card.localeCompare(b.card) : b.card.localeCompare(a.card)));
+	}
+
+	static byWeight(cards: DivcordRecordAndWeight[], order: Order): void {
+		cards.sort((a, b) =>
+			order === 'asc' ? Number(a.weight) - Number(b.weight) : Number(b.weight) - Number(a.weight)
+		);
+	}
+}
