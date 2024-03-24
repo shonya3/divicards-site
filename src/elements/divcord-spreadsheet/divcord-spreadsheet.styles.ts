290e6fd13ea7918292e0ad8b17f39e81c3b86d0f
shonya3 <poeshonya3@gmail.com>
2024-03-24 17:18:38 +0300

    create file for styles

---
+++ src/elements/divcord-spreadsheet/divcord-spreadsheet.styles.ts
@@ -0,0 +1,139 @@
+import { css } from 'lit';
+
+export const styles = css`
+	#root {
+		height: calc(100vh - 100px);
+		overflow-y: scroll;
+		width: fit-content;
+		padding: 2rem;
+	}
+
+	.table {
+		border-collapse: collapse;
+		border: 1px solid rgba(140, 140, 140, 0.3);
+		table-layout: fixed;
+		width: 1250px;
+		font-size: 14px;
+		position: relative;
+	}
+
+	.tbody {
+		width: 1450px;
+		display: table-row-group;
+		transform: translateX(-1px);
+	}
+
+	.thead__headings {
+		position: sticky;
+		top: -40px;
+		background-color: black;
+		z-index: 9999;
+	}
+
+	.col-id.td {
+		border-left: none;
+	}
+
+	.col-verify.td {
+		border-right: none;
+	}
+
+	.col-id {
+		width: 50px;
+	}
+	.col-card {
+		width: 200px;
+	}
+	.col-weight {
+		width: 100px;
+	}
+	.col-tag {
+		word-break: break-word;
+		width: 200px;
+	}
+	.col-confidence {
+		width: 100px;
+	}
+	.col-remaining-work {
+		width: 100px;
+	}
+	.col-sources {
+		width: 400px;
+	}
+	.col-verify {
+		width: 300px;
+	}
+
+	.sources {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+		margin-top: 0.25rem;
+		column-gap: 0.5rem;
+	}
+
+	.sources-maps {
+		display: flex;
+		flex-direction: row;
+		flex-wrap: wrap;
+	}
+
+	.confidence {
+		position: relative;
+		text-transform: uppercase;
+		font-size: 13px;
+	}
+
+	.confidence--done {
+		background-color: green;
+	}
+	.confidence--ok {
+		background-color: #93c47d;
+		color: black;
+	}
+	.confidence--none {
+		background-color: red;
+	}
+	.confidence--low {
+		background-color: #f1c232;
+		color: black;
+	}
+
+	.th {
+		font-size: 14px;
+	}
+
+	.th,
+	.td {
+		padding: 0.4rem;
+		border: 1px solid rgba(160, 160, 160, 0.2);
+		text-align: center;
+	}
+
+	.header-with-icon {
+		display: flex;
+		justify-content: center;
+		align-items: center;
+		gap: 0.4rem;
+	}
+
+	.td-notes {
+		text-align: left;
+	}
+
+	.td-weight {
+		font-weight: 700;
+		font-size: 20px;
+	}
+
+	.ordered-by {
+		color: yellow;
+	}
+
+	@media (width < 25rem) {
+		.th,
+		.td {
+			padding: 0.4rem;
+		}
+	}
+`;
