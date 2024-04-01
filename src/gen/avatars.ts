046174cca8f0e117ee604df25ff2e40ac9939348
shonya3 <poeshonya3@gmail.com>
2024-04-01 19:35:08 +0300

    update gen

--- src/gen/avatars.ts
+++ src/gen/avatars.ts
@@ -1,3 +1,4 @@
+export type DiscordUsername = keyof typeof DISCORD_AVATARS;
 export const DISCORD_AVATARS = {
 	nerdyjoe: {
 		username: 'nerdyjoe',
@@ -9,6 +10,19 @@ export const DISCORD_AVATARS = {
 		color: 'rgb(52, 152, 219)',
 		url: 'https://cdn.discordapp.com/avatars/89395995351220224/7407cb784d48cee6661e7dcb539fdcbd.webp',
 	},
+	deathbeam: {
+		username: 'deathbeam',
+		color: 'rgb(46, 204, 113)',
+		url: 'https://cdn.discordapp.com/avatars/317319692823953409/eb977a8aabab572e3ff718e85ed0a211.webp',
+	},
+	kroIya: {
+		username: 'kroIya',
+		color: 'rgb(46, 204, 113)',
+		url: 'https://cdn.discordapp.com/avatars/182893458858442762/02ca35e040e0e62a9ed643c97085f29e.webp',
+	},
+	tikiheme: {
+		username: 'tikiheme',
+		color: 'rgb(52, 152, 219)',
+		url: 'https://cdn.discordapp.com/avatars/636016169148481547/7332fcbe7461626a0e8b64ae00256670.webp',
+	},
 };
-
-export type DiscordUsername = keyof typeof DISCORD_AVATARS;
