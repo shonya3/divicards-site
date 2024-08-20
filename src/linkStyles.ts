c66267e338bb77679948f52229ef2bff73434514
shonya3 <poeshonya3@gmail.com>
2024-08-20 14:26:26 +0300

    add :where

--- src/linkStyles.ts
+++ src/linkStyles.ts
@@ -1,16 +1,15 @@
 import { css } from 'lit';
 
 export const linkStyles = css`
-	a:link {
+	:where(a:link) {
 		text-decoration: none;
 	}
 
-	a,
-	a:visited {
+	:where(a:link, a:visited) {
 		color: var(--source-color, #bbbbbb);
 	}
 
-	a:hover {
+	:where(a:hover) {
 		color: var(--link-color-hover, skyblue);
 		text-decoration: underline;
 	}
