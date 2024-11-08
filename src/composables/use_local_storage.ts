d83f30e5e29f83d0b67776dbf2d8b7c1a2768356
shonya3 <poeshonya3@gmail.com>
2024-11-08 23:15:52 +0300

    add try-catch for json.parse

--- src/composables/use_local_storage.ts
+++ src/composables/use_local_storage.ts
@@ -4,13 +4,23 @@ import { Registry } from '../storage';
 
 export function use_local_storage<Key extends keyof Registry>(storage_key: Key, default_value: Registry[Key]) {
 	const from_storage = localStorage.getItem(storage_key);
-	const value: Registry[Key] = from_storage === null ? default_value : JSON.parse(from_storage);
 
-	const signal_value = signal(value);
+	let value: Registry[Key];
+	if (from_storage === null) {
+		value = default_value;
+	} else {
+		try {
+			value = JSON.parse(from_storage);
+		} catch (err) {
+			value = default_value;
+		}
+	}
+
+	const _signal = signal(value);
 
 	effect(() => {
-		localStorage.setItem(storage_key, JSON.stringify(signal_value.get()));
+		localStorage.setItem(storage_key, JSON.stringify(_signal.get()));
 	});
 
-	return signal_value;
+	return _signal;
 }
